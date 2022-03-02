28dbdf8f6bb972078c2665b8164efcd19283ce03
Georgi Minkov <georgiminkov@Georgis-MacBook-Pro.local>
2022-03-02 12:38:07 +0200

    collection

---
+++ FMICourse/src/week02/collection/CollectionExample.java
@@ -0,0 +1,74 @@
+package week02.collection;
+
+import java.util.*;
+
+public class CollectionExample {
+
+    private void defineListCollection() {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        printCollection(list);
+
+        list = Arrays.asList("four", "five", "six");
+        printCollection(list);
+
+        list = new LinkedList<>();
+        list.add("seven");
+        list.add("eight");
+        list.add("nine");
+        printCollection(list);
+    }
+
+    private <T> void printCollection(final Collection<T> collection) {
+        for (T element : collection) {
+            System.out.println(element);
+        }
+
+        System.out.println("----------with explicit iterator-------------");
+        Iterator<T> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+
+        System.out.println("-----------------------");
+
+
+//        collection = new ArrayList<>(); you cannot do this when final keyword is present
+    }
+
+    private void defineMapCollection() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        printMap(map);
+
+        if (map.containsKey(Integer.valueOf("2"))) {
+            System.out.println("there is a key with value 2");
+        }
+
+        String value = map.get(Integer.valueOf(2));
+        System.out.println("value of 2 -> " + value);
+
+        value = map.get(4);
+        System.out.println("value of 2 -> " + value);
+    }
+
+    private void printMap(final Map<Integer, String> map) {
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(String.format("%d - %s", entry.getKey(), entry.getValue()));
+        }
+
+//        map.entrySet().stream().forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        CollectionExample collectionExample = new CollectionExample();
+        collectionExample.defineListCollection();
+
+        collectionExample.defineMapCollection();
+    }
+}
